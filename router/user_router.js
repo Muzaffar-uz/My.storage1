8fbcb778577e83fb7be96456d913e7f00d7ee86f
Muzaffar To'raqulov <turakulovmuzaffar1995@gmail.com>
2024-08-05 20:41:29 +0500

    main

--- router/user_router.js
+++ router/user_router.js
@@ -1,14 +1,14 @@
 const router = require('express').Router()
-
+;;;;;
 const Usercontroller = require('../controller/user_controller')
-const {protect,role} = require('../middleware/auth-middleware')
+const {protect,role} = require('../middleware/auth-middleware');;
 
-
+;
 router.get('/role',protect,role("Admin"),Usercontroller.getUser)
-router.post('/role',Usercontroller.postUser)
+router.post('/role',Usercontroller.postUser);
 router.put('/role/:id',Usercontroller.updetUser)
-router.delete('/role/:id',Usercontroller.delteUser)
-router.post('/auth',Usercontroller.auth)
+router.delete('/role/:id',Usercontroller.delteUser);
+router.post('/auth',Usercontroller.auth);
 
 
 module.exports = router;
